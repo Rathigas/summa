a58aa388daa89133bb17f864ecf6f792f957233d
Rathigas <rathiga@commusoft.co.uk>
2019-10-01 16:50:41 +0530

    testing

--- Github/src/test/java/Gitpack/Classgit.java
+++ Github/src/test/java/Gitpack/Classgit.java
@@ -5,7 +5,7 @@ public class Classgit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Github checking");
+		System.out.println("Github checkixcvcxvng");
 	}
 
 }
